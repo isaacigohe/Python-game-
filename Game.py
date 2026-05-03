610382f3dce8a9180c3465a591da82130b2039ca
isaacigohe <isaacigohe120@gmail.com>
2026-05-03 19:56:14 +0300

    settings levels

--- Game.py
+++ Game.py
@@ -1,1 +1,10 @@
 import random
+
+def choose_difficulty():
+    """Let player choose difficulty level"""
+    print("\nSelect Difficulty Level:")
+    print("1. Easy (10 tries)")
+    print("2. Medium (7 tries)")
+    print("3. Hard (5 tries)")
+    
+    
